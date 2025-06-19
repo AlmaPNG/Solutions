8c84c536c8d8a219105b15664b3f631b8bfeaa4d
Alma <almaudeborg@gmail.com>
2025-06-19 09:34:15 +0200

    Finished cars quiz

--- S0580_oop_terms_quiz.py
+++ S0580_oop_terms_quiz.py
@@ -16,29 +16,29 @@ Tilføj oop-relaterede kommentarer til denne kode.
 
 class Building:  #class definition
     def __init__(self, area, floors, value): #constructor
-        self.area = area
-        self.floors = floors
-        self._value = value
+        self.area = area #attribute
+        self.floors = floors # attribute
+        self._value = value #protected attribute
 
-    def renovate(self):
+    def renovate(self): #method
         print("Installing an extra bathroom...")
         self._adjust_value(10)
 
-    def _adjust_value(self, percentage):
+    def _adjust_value(self, percentage): #protected method
         self._value *= 1 + (percentage / 100)
         print(f'Value has been adjusted by {percentage}% to {self._value:.2f}\n')
 
 
-class Skyscraper(Building):
+class Skyscraper(Building): #inheritance
 
-    def renovate(self):
+    def renovate(self): #method
         print("Installing a faster elevator.")
         self._adjust_value(6)
 
 
-small_house = Building(160, 2, 200000)
-skyscraper = Skyscraper(5000, 25, 10000000)
+small_house = Building(160, 2, 200000) #object definition
+skyscraper = Skyscraper(5000, 25, 10000000) #object definition
 
-for building in [small_house, skyscraper]:
+for building in [small_house, skyscraper]: # polymorphism
     print(f'This building has {building.floors} floors and an area of {building.area} square meters.')
     building.renovate()
